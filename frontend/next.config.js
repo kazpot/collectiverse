4b37b632fd5696e4ddbb317e402caeac86c3991c
kazpot <knman7@hotmail.com>
2023-12-07 21:23:28 +0900

    Modify next config

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -14,11 +14,7 @@ module.exports = {
       {
         source: '/api/:path*',
         headers: [
-          { key: 'Access-Control-Allow-Credentials', value: 'true' },
-          {
-            key: 'Access-Control-Allow-Origin',
-            value: 'https://collectiverse-git-main-anon.vercel.app',
-          },
+          { key: 'Access-Control-Allow-Origin', value: '*' },
           { key: 'Access-Control-Allow-Methods', value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT' },
           {
             key: 'Access-Control-Allow-Headers',
