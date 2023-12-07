e205e46b1ff11c80772456b6b93025a394900095
kazpot <knman7@hotmail.com>
2023-12-08 00:21:30 +0900

    Allow cancellation without placing a bid

--- frontend/components/card/NFTCreatedCard.tsx
+++ frontend/components/card/NFTCreatedCard.tsx
@@ -104,7 +104,7 @@ export default function NFTCreatedCard({
         <CardContent sx={{ justifyContent: 'center', display: 'flex' }}>
           {currentUserAddress.toLowerCase() === item.maker.toLowerCase() && (
             <Button
-              disabled={item.bestPrice != ''}
+              disabled={item.bestBidder != ''}
               onClick={() => cancel(item)}
               variant='outlined'
               sx={{
