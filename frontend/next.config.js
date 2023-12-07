cf569ce747198a1576c6fb584ea3abd31f9b67bd
kazpot <knman7@hotmail.com>
2023-12-07 12:03:54 +0900

    Allow cors

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -3,4 +3,21 @@ module.exports = {
   images: {
     domains: ['ipfs.infura.io', 'ipfs.io', 'gateway.pinata.cloud', 'ipfs.filebase.io'],
   },
+  async headers() {
+    return [
+      {
+        source: '/api/:path*',
+        headers: [
+          { key: 'Access-Control-Allow-Credentials', value: 'true' },
+          { key: 'Access-Control-Allow-Origin', value: '*' },
+          { key: 'Access-Control-Allow-Methods', value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT' },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value:
+              'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+          },
+        ],
+      },
+    ];
+  },
 };
